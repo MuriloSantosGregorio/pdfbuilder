1c8930582e1e7ae4b3d169b556537fa61f1cc95d
Murilo Dos Santos Gregorio <50981737870@info.local>
2019-11-11 10:28:25 -0300

    Upload do test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { PDFViewer } from '@react-pdf/renderer';
+import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
+import { maxHeaderSize } from 'http';
+
+// Create styles
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: 'row',
+    backgroundColor: '#FFFFFF'
+  },
+  paget: {
+    flexDirection: 'row',
+    backgroundColor: '#000000',
+    color: '#FFFFFF'
+    
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1
+  },
+  pagina: {
+    height: 840,
+    width: 594
+  }
+});
+
+// Create Document Component
+const MyDocument = () => (
+  <Document>
+    <Page style={styles.page} size={[594,840]}>
+      <View style={styles.section}>
+        <Text>Section #1</Text>
+      </View>
+      <View style={styles.section}>
+        <Text>Section #2</Text>
+      </View>
+    </Page>
+    <Page style={styles.paget} size={[594,840]}>
+      <View style={styles.section}>
+        <Text>Section #1</Text>
+      </View>
+      <View style={styles.section}>
+        <Text>Section #2</Text>
+      </View>
+    </Page>
+    
+  </Document>
+);
+
+const App = () => (
+  <PDFViewer style={styles.pagina}>
+    <MyDocument/>
+  </PDFViewer>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
